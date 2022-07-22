9270bf36a5f6a0003763713a9859e6284ff4c6c1
Nauã Holanda <naua.holanda@gmail.com>
2022-07-21 21:35:42 -0300

    #1 App Created

---
+++ src/main/java/br/com/nauaholanda/bomHotel/BomHotelApplication.java
@@ -0,0 +1,13 @@
+package br.com.nauaholanda.bomHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomHotelApplication.class, args);
+	}
+
+}
